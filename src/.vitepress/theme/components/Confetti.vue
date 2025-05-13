ad85494ba7e5df9ebeeb1c726c979c13903fbb8b
Donny <donnymoving@gmail.com>
2025-05-13 21:25:44 +0800

    Update Confetti.vue

--- src/.vitepress/theme/components/Confetti.vue
+++ src/.vitepress/theme/components/Confetti.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import confetti from "canvas-confetti";
 
-onMounted(() => {
-  /* 纸屑 */
-  confetti({
-    particleCount: 100,
-    spread: 170,
-    origin: { y: 0.6 },
-  });
-})
+/* 纸屑 */
+confetti({
+  particleCount: 100,
+  spread: 170,
+  origin: { y: 0.6 },
+});
 </script>
